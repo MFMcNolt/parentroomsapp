951208e9cd4a16bcb5c8a5134c0da47206ec5359
MFMcNolt <mfergusonmcnulty@gmail.com>
2024-12-02 14:15:58 +1100

    added in logo and works

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
+import 'react-native-gesture-handler';
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, StyleSheet, ActivityIndicator, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './src/services/firebase';
 import AuthScreen from './src/screens/AuthScreen';
 import HomeScreen from './src/screens/HomeScreen';
+
+
+const Stack = createStackNavigator();
+
+const LogoTitle = () => (
+  <Image
+    source={require('./assets/images/logo.png')}
+    style={{ height: 40, width: 180 }}
+    resizeMode="contain"
+  />
+);
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -26,7 +40,39 @@ const App = () => {
     );
   }
 
-  return user ? <HomeScreen /> : <AuthScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitle: props => <LogoTitle {...props} />,
+          headerStyle: {
+            backgroundColor: '#fff',
+            elevation: 1,
+            shadowOpacity: 0.3,
+          },
+          headerTitleAlign: 'center',
+        }}
+      >
+        {!user ? (
+          <Stack.Screen 
+            name="Auth" 
+            component={AuthScreen}
+            options={{
+              headerShown: true,
+            }}
+          />
+        ) : (
+          <Stack.Screen 
+            name="Home" 
+            component={HomeScreen}
+            options={{
+              headerShown: true,
+            }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
